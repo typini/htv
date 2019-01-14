b54cbc16b1a38d3e461373b945c96e575ef8d3c9
typini <typini@gmail.com>
2019-01-14 15:37:17 -0700

    Create HTV.Master.cs

---
+++ HTV.Master.cs
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////
+// File:  HTV.Master.cs
+// Program Name:  HTV_v2.0
+// Programmer:  Tyree Pini
+// Group:  Team D
+// Course:  CIS470 Senior Project
+// Date:  19 April 2017
+// Website:  http://htvteamd.azurewebsites.net/
+///////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HTV
+{ 
+    public partial class Webform928 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                        
+        }
+    }
+}
