e00feaa7cca91e9dc229b88dbefcec2ab07d2284
typini <typini@gmail.com>
2019-01-14 15:36:34 -0700

    Create GenerateReports.aspx.cs

---
+++ GenerateReports.aspx.cs
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////
+// File:  GenerateReports.aspx.cs
+// Program Name:  HTV_v2.0
+// Programmer:  Tyree Pini
+// Group:  Team D
+// Course:  CIS470 Senior Project
+// Date:  19 April 2017
+// Website:  http://htvteamd.azurewebsites.net/
+///////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HTV
+{
+    public partial class WebForm12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Warnings.Text = "This page is out of the scope for this demonstration and does not function.";
+        }
+    }
+}
